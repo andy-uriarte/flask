8e7ceeac7f483a541a6735527349cc604978806b
Andy Uriarte <andy@codelitt.com>
2019-05-31 16:23:23 -0500

    Error and success messages now in for registration

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 import os
 
-from flask import Flask, session, jsonify, redirect, url_for, render_template, request
+from flask import Flask, flash, session, jsonify, redirect, url_for, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from pprint import pprint
 from dbsetup import createUsers, createBooks, createReviews
 from importBooks import importBooks
-
+from models import User
 
 app = Flask(__name__)
 
@@ -42,6 +42,7 @@ def index():
 
     if 'logged_in' not in session:
         session['logged_in'] = False
+        return redirect(url_for('login'))
 
     if session['logged_in']:
             return '<h1>You are logged in!</h1>'
@@ -49,6 +50,43 @@ def index():
         session.pop('logged_in', None)
         return redirect(url_for('login'))
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("layout.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        userEmail = request.form.get("email")
+        userPassword = request.form.get("password")
+        userID = db.execute("SELECT id FROM users WHERE email = :email AND password = :password",
+            {"email": userEmail, "password": userPassword}).fetchone()
+        if userID is None:
+            return "Wrong credentials"
+        else:
+            session['logged_in'] = True
+            flash('Success')
+            return render_template("login.html", message="You are now logged in")
+
+@app.route("/logout")
+def logout():
+        session.pop('logged_in', None)
+        print('logging you out')
+        return redirect(url_for('index'))
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        newUsername = request.form.get("username")
+        newEmail = request.form.get("email")
+        newPassword = request.form.get("password")
+
+        # Use class to register the user
+        try:
+            newUser = User()
+            newUser.addUser(newUsername, newEmail, newPassword)
+        except Exception as e:
+            return render_template("register.html", error = str(e))
+        else:
+            session['logged_in'] = True
+            return render_template("register.html", message="Account succesfully created!")
